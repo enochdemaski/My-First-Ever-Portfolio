ad89e77da98eefefb0e48c2b4fd20a84e271f1b7
Maski-hub <enochokey9@gmail.com>
2025-11-19 14:26:42 +0100

    added icons on hero section

--- src/component/homeherosection/Hero.jsx
+++ src/component/homeherosection/Hero.jsx
@@ -6,7 +6,7 @@ function Hero() {
   return (
     <section className="">
       <div id="hero" className="flex-container h-screen ">
-        <div className="ml-4 mt-10 p-5 text-center ">
+        <div className="ml-5 mt-10 p-5 text-center ">
           <span className=" font-bold text-3xl ">Hello,</span>
           <h2 className="text-2xl w-89  ">
             I'm{" "}
@@ -18,11 +18,19 @@ function Hero() {
           <h1 className="text-2xl">
             A <span className="text-blue-600">Web Developer.</span>{" "}
           </h1>
-          <p className=" text-shadow-md mt-4">
+          <p className=" text-shadow-md mt-4 ">
             I am skiled Web Developer with expierences in creating visually
-            appealing and user-friendly websites.
+            appealing and user-friendly websites and Web App.
           </p>
-          <button className="rounded-full mt-4 m-auto text-sm flex gap-10">
+
+          <div className=" mt-3 pt-1 flex gap-3 items-center justify-center w-47 m-auto text-black rounded-full bg-white h-7">
+            <i class="fa fa-envelope-open" aria-hidden="true"></i>
+            <i class="fa fa-envelope-open" aria-hidden="true"></i>
+            <i class="fa fa-envelope-open" aria-hidden="true"></i>
+            <i class="fa fa-envelope-open" aria-hidden="true"></i>
+          </div>
+
+          <button className="flex gap-10 m-auto mt-4">
             <a
               href="#"
               className=" text-black p-2 rounded-4xl w-28 border border-transparent bg-white shadow-[0_0_20px_white]"
