5638862bfb8a6b443d57d8613cae052eb697a692
Maski-hub <enochokey9@gmail.com>
2025-11-22 09:20:01 +0100

    changed mt for cards

--- src/component/CardComponent/Card.jsx
+++ src/component/CardComponent/Card.jsx
@@ -3,7 +3,7 @@ import "./Card.css";
 
 export default function Card() {
   return (
-    <div className=" bg-blue flex flex-wrap gap-4 justify-center items-center mt-4  lg: ">
+    <div className=" bg-blue flex flex-wrap gap-4 justify-center items-center mt-3">
       <div className="cardcontainer shadow-[0_0_20px_white]  w-96 bg-gray-100 h-80 rounded-3xl  md:bases-1/2 lg:basis-1/5 lg:w-[100]    ">
         <div className="child-card-container bg-gray-200 border-b-gray-950 border-2 p-2 h-30 rounded-3xl ">
           <p className="text-black capitalize text-center">web development</p>
